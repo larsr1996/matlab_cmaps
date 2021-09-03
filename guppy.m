b8555cd632be31fb54193ebeb6ec73829b584735
Lars Reichwein <lars.reichwein@hhu.de>
2021-09-04 01:09:33 +0200

    Update guppy.m

--- guppy.m
+++ guppy.m
@@ -8,11 +8,6 @@ else
     end
 end
 
-% p = [0, 0.5, 1]; % percentages at which colors occur
-% r = [250, 88, 30] / 255; % red values, bottom to top
-% g = [144, 8, 203] / 255; % green values, bottom to top
-% b = [79, 114, 191] / 255; % blue values, bottom to tops
-
 p = [0, 0.25, 0.5, 0.75, 1]; % percentages at which colors occur
 r = [250, 250, 88, 57, 30] / 255; % red values, bottom to top
 g = [144, 33, 8, 105, 203] / 255; % green values, bottom to top
@@ -20,7 +15,6 @@ b = [79, 23, 114, 233, 191] / 255; % blue values, bottom to top
 
 pq = linspace(0,1,m); % create finer spacing with m steps
 method = 'makima'; % interpolation method
-% method = 'makima'; % alternative interpolation method
 
 % interpolate RGB arrays
 rr = interp1(p, r, pq, method);
